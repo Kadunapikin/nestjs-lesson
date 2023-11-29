739867dd89456e7dadcb6225ea6f9ee4f55ae70d
kadunapikin <ibakoshi27@gmail.com>
2023-11-29 16:11:38 +0100

    add a /user post route to add a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,33 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')  //this is like the parent route to handle /user request
 export class UsersController {
     /*
-    POST /users
     PATCH /users/:id
     DELETE /users/:id
     */
 
     @Get()      // GET /ussers
     findAll() {
+        // return ['daniel', 'Emmanuel', 'Bobai', 'Isaac']
         return []
     }
+
+    //any route before the params route will be seein as an id thats why is very important to put all routes before the params route
+    // @Get('interns')      // GET /ussers/interns
+    // findAllInterns() {
+    //     return ['Bobai', 'Isaac']
+    // }
 
     @Get(':id')    // GET /users/:id
     findOne(@Param('id') id: string) {
         return { id }
     }
+
+    @Post() //     POST /users
+    create(@Body() user: {}) {
+        return user
+    } 
 
 
 
