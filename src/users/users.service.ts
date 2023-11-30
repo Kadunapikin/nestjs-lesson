6fb0a70d7be9762f67fa2fbfd6d58aec263ca5d8
kadunapikin <ibakoshi27@gmail.com>
2023-11-30 18:25:19 +0100

    update the create method in users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -48,7 +48,7 @@ export class UsersService {
     }
 
     create(user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
-        const usersByHighestId = [...this.users].sort((a,b) => b.id = a.id)
+        const usersByHighestId = [...this.users].sort((a,b) => b.id - a.id)
         const newUser = {
             id: usersByHighestId[0].id + 1, ...user
         }
